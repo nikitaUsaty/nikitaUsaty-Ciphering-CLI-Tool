9a9a87bbff055b6032f9a267d68945ec76554158
nikitausaty <nikitausatiy@yandex.ru>
2021-11-21 17:10:30 +0300

    feat: divide tests into groups and add descriptions for each group

--- README.md
+++ README.md
@@ -1,42 +1,9 @@
-# nikitaUsaty-Ciphering-CLI-Tool
+# nikitaUsaty-Ciphering-CLI-Tool-Testing
 
-## About
+## How to run test
 
-This tool lets you encode and/or decode a text by Caesar/ROT8/Atbash ciphers.
-
-## Install
-
-Open command line in the folder where you want to clone this program. Clone repository using command below.
+In command line type
 
 ```bash
-git clone https://github.com/Hellsingi/CLI-tool.git
-git checkout Caesar_cipher_cli
+npm test
 ```
-
-Install dependencies
-
-## How it works
-
-The CLI tool is accepted 3 options (short alias and full name):
-
-1.  **-c, --config**: config for ciphers Config is a string with pattern {XY(-)}n, where:
-    X is a cipher mark:
-    C is for Caesar cipher (with shift 1)
-    A is for Atbash cipher
-    R is for ROT-8 cipher
-    Y is flag of encoding or decoding (mandatory for Caesar cipher and ROT-8 cipher and should not be passed Atbash cipher)
-    1 is for encoding
-    0 is for decoding
-    **required!!!**
-
-2.  **-i, --input**: a path to input file**optional**
-3.  **-o, --output**: a path to output file **optional**
-
-## Usage example:
-
-```bash
-node my_ciphering_cli -c "C1-C1-R0-A" -i "./input.txt" -o "./output.txt"
-```
-
-input.txt This is secret. Message about "_" symbol!
-output.txt Myxn xn nbdobm. Tbnnfzb ferlm "_" nhteru!
--- test/args.spec.js
+++ test/args.spec.js
@@ -1,31 +1,37 @@
 const parseArgs = require('../bin/args')
 
-test('should return undefined', () => {
-  expect(parseArgs(['-c', 'C1-C1-R0-A', '-i', './input.txt', '-o', './output.txt'])).toEqual({
-    config: 'C1-C1-R0-A',
-    input: './input.txt',
-    output: './output.txt',
+describe('Parsing arguments tests', () => {
+  test(`['-c', 'C1-C1-R0-A', '-i', './input.txt', '-o', './output.txt'] should be equal {
+  config: 'C1-C1-R0-A',
+  input: './input.txt',
+  output: './output.txt',
+}`, () => {
+    expect(parseArgs(['-c', 'C1-C1-R0-A', '-i', './input.txt', '-o', './output.txt'])).toEqual({
+      config: 'C1-C1-R0-A',
+      input: './input.txt',
+      output: './output.txt',
+    })
   })
-})
 
-it('case "ghfhfj" should throw an error', async () => {
-  const mockExit = jest.spyOn(process, 'exit').mockImplementation((number) => {
-    throw new Error('process.exit: ' + number)
+  it('case "ghfhfj" should throw an error', async () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation((number) => {
+      throw new Error('process.exit: ' + number)
+    })
+    expect(() => {
+      parseArgs(['ghfhfj'])
+    }).toThrow()
+    expect(mockExit).toHaveBeenCalledWith(1)
+    mockExit.mockRestore()
   })
-  expect(() => {
-    parseArgs(['ghfhfj'])
-  }).toThrow()
-  expect(mockExit).toHaveBeenCalledWith(1)
-  mockExit.mockRestore()
-})
 
-it('case "-c C1-C1-R0-A -i -o ./output.txt" should throw an error', async () => {
-  const mockExit = jest.spyOn(process, 'exit').mockImplementation((number) => {
-    throw new Error('process.exit: ' + number)
+  it('case "-c C1-C1-R0-A -i -o ./output.txt" should throw an error', async () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation((number) => {
+      throw new Error('process.exit: ' + number)
+    })
+    expect(() => {
+      parseArgs(['-c', 'C1-C1-R0-A', '-i', '-o', './output.txt'])
+    }).toThrow()
+    expect(mockExit).toHaveBeenCalledWith(1)
+    mockExit.mockRestore()
   })
-  expect(() => {
-    parseArgs(['-c', 'C1-C1-R0-A', '-i', '-o', './output.txt'])
-  }).toThrow()
-  expect(mockExit).toHaveBeenCalledWith(1)
-  mockExit.mockRestore()
 })
